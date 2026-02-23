327c112b8c90cd6a0aa3cd8cac64eafc68b2cd9b
tamjidhasan8 <tamjidhasan.jm@gmail.com>
2026-02-24 00:46:47 +0600

    Initial commit - Inventory Management System setup

---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
+
+Route::get('/', function () {
+    return redirect()->route('login');
+});
+
+Route::get('/dashboard', function () {
+    return view('admin.index');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
+
+Route::get('/admin/logout', [AdminController::class, 'AdminLogout'])->name('admin.logout');
